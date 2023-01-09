4c6097ef50c3b834fe27ecef58a07ed7d455db46
Hamilton <>
2023-01-09 11:11:49 +0100

    add ErrorBoudry

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import CardList from "../components/CardList";
 import Searchbox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry";
 import "./App.css";
 
 class App extends React.Component {
@@ -35,7 +36,9 @@ class App extends React.Component {
         <h1 className="f1 ">RobotFriends</h1>
         <Searchbox searchChange={this.onSearchChange} />
         <Scroll>
-          <CardList robots={filteredRobots} />
+          <ErrorBoundry>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundry>
         </Scroll>
       </div>
     );
